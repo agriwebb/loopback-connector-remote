197e0d500939932bd14c6478ce77b3fc2a01162e
Kenny Sabir <kenny.sabir@agriwebb.com>
2017-10-18 09:35:04 +1100

    revert back to options

--- test/helper.js
+++ test/helper.js
@@ -48,7 +48,7 @@ function createRemoteDataSourceWithOptions(remoteApp, options) {
   return loopback.createDataSource({
     url: 'http://anyURL.com',
     connector: remoteConnector,
-    remoting: options
+    options: options
   });
 }
 
